ffd4964a49fec20079b436ea57a4a0d14f77cfe8
rebecca.anjos30@gmail.com <rebecca.anjos30@gmail.com>
2025-01-16 11:46:54 -0300

    instalação do handlebars e do nodemon

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
 const express = require("express")
+const exphbs = requise("express-handlebars")
+
 const app = express()
+
+app.engine('handlebars', exphbs.engine())
+app.set('view engine', 'handlebars')
+
 app.get('/', (requisicao,resposta) => {
     resposta.send("Olá, mundo!")
 })
